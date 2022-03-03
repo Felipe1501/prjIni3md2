f54162df5b084d5d60fff38b777d0fb39c64515d
etec adolpho berezin <e107ti@cps.sp.gov.br>
2022-03-03 15:09:48 -0300

    adicionando input e afins

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.texto}>METEU ESSA?</Text>
-      <StatusBar style="auto" />
+      <View style={styles.bloco}>
+        <Text style={styles.textoBloco}>QUE PAPINHO 1</Text>
+        <TextInput 
+          style={styles.input}
+          keyboardType="numeric"
+      />
+    </View>
+    <View style={styles.bloco}>
+        <Text style={styles.textoBloco}>QUE PAPINHO 2</Text>
+        <TextInput 
+          style={styles.input}
+          keyboardType="numeric"
+        />
+      </View>
+      <View style={styles.bloco}>
+        <TouchableOpacity style={styles.botao}>
+            <Text style={styles.textoBotao}>Somar</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -13,12 +31,35 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#DDA0DD',
+    backgroundColor: '#FF69B4',
     alignItems: 'center',
     justifyContent: 'center',
   },
   texto:{
-    color: '#FFC0CB',
+    color: '#7B68EE',
     fontSize: 30
+  },
+  textoBloco:{
+    fontSize:20,
+  },
+  input:{
+    borderColor:'#7B68EE',
+    borderWidth:2,
+    fontSize:30,
+    width:'80%'
+  },
+  bloco:{
+    width:'100%',
+    alignItems:'center',
+    marginTop:30
+  },
+  botao:{
+    backgroundColor:'#7B68EE',
+    width:'80%',
+    textAlign:'center'
+  },
+  textoBotao:{
+    color:"#fff",
+    fontSize:30
   }
 });
